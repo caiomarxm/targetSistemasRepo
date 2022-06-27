e42408c6bf5895409312ea7eebdf7d853d6afa14
Caio Martins <80432903+caiomarxm@users.noreply.github.com>
2022-06-27 17:18:31 -0300

    feat: Questao 4

---
+++ questao4.py
@@ -0,0 +1,30 @@
+invoicing = {
+        'SP': 67836.43, 
+        'RJ': 36678.66,
+        'MG': 29229.88,
+        'ES': 27165.48,
+        'Outros': 19849.53
+    }
+
+def getTotal(invoicing):
+    summation = 0
+    for value in invoicing.values():
+        summation += value
+    return summation
+
+def getPercentual(state):
+    return invoicing[state]/getTotal(invoicing)
+
+if __name__ == '__main__':
+    
+    total = getTotal(invoicing)
+    print('Para qual estado deseja calcular o percentual?')
+    print('Obs.: Inserir sigla (Exemplo: "SP" para São Paulo)')
+    state = input().upper()
+
+    try:
+        percentual = getPercentual(state)
+        print(f'{state} representa {percentual*100:.2f}% do faturamento mensal.')
+    except:
+        print('Não há informações suficientes para concluir requisição.')
+        print('Estados disponíveis: SP, RJ, MG e ES')
