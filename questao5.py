f4b7b895d497622138f51951e50cfafe777bdf97
Caio Martins <80432903+caiomarxm@users.noreply.github.com>
2022-06-27 17:25:41 -0300

    feat: Questao 5

---
+++ questao5.py
@@ -0,0 +1,12 @@
+def revertString(string):
+    reversedString = ''
+    for i in range(len(string)):
+        reversedString += string[-(i+1)]
+    return reversedString
+
+
+if __name__ == '__main__':
+    print(f'Insira string para inversão: ')
+    string = str(input())
+    print('String invertida: ')
+    print(revertString(string))
