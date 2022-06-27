26edd42d57fafc0689f476b428510ccc464b2295
Caio Martins <80432903+caiomarxm@users.noreply.github.com>
2022-06-27 16:30:56 -0300

    feat: Questao 2

---
+++ questao2.py
@@ -0,0 +1,20 @@
+def getFibonacci(number):
+    fibonacci = [0, 1]
+    while fibonacci[-1] < number:
+        nextNumber = fibonacci[-1] + fibonacci[-2]
+        fibonacci.append(nextNumber)
+    return fibonacci
+
+def checkFibonacci(number):
+    fibonacci = getFibonacci(number)
+    return number in fibonacci
+
+
+if __name__ == '__main__':
+    print('Informe o número desejado: ')
+    number = int(input())
+    result = checkFibonacci(number)
+    if result:
+        print(f'O número {number} está contido na sequência.')
+    else:
+        print(f'O número {number} NÃO faz parte da sequência.')
